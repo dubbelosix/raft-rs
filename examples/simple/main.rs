e4d2f665fbc892082fba67717abf16e6dacfcb81
dubbelosix <dub@006.com>
2023-12-13 12:13:48 +0530

    simple example with network

---
+++ examples/simple/main.rs
@@ -0,0 +1,313 @@
+use std::sync::mpsc::{self, Receiver, RecvTimeoutError, Sender, TryRecvError};
+use std::time::{Duration, Instant, UNIX_EPOCH};
+use std::{str, thread};
+use std::collections::HashMap;
+use std::env;
+use std::str::FromStr;
+use slog::Logger;
+use actix_web::Responder;
+use futures::channel::oneshot;
+
+use protobuf::Message as PbMessage;
+use raft::storage::MemStorage;
+use raft::{prelude::*, StateRole};
+use slog::o;
+use slog::{Drain, Level};
+use std::io::{self};
+
+use actix_web::{web, App, HttpResponse, HttpServer};
+
+const NODE_LIST: [&'static str; 3] = ["127.0.0.1:9000","127.0.0.1:9001", "127.0.0.1:9002"];
+
+enum Prop {
+    Data(Vec<u8>,oneshot::Sender<Option<()>>,),
+    Conf(ConfChange)
+}
+
+use std::net::UdpSocket;
+use std::io::{ErrorKind};
+
+fn raft_send(addr: &str, data: &[u8]) -> io::Result<()> {
+    let socket = UdpSocket::bind("0.0.0.0:0")?; // Bind to a random port
+    socket.send_to(data, addr)?;
+    Ok(())
+}
+
+fn raft_listen(addr: &str, sender: Sender<Message>) -> io::Result<()> {
+    let socket = UdpSocket::bind(addr)?;
+    let mut buffer = [0; 65507]; // Maximum UDP packet size
+
+    loop {
+        match socket.recv_from(&mut buffer) {
+            Ok((size, _src)) => {
+                let message = Message::parse_from_bytes(&buffer[..size]).unwrap();
+                sender.send(message).unwrap();
+            }
+            Err(ref e) if e.kind() == ErrorKind::WouldBlock => {
+                // Handle the case where there's no data to be read
+                continue;
+            }
+            Err(e) => return Err(e),
+        }
+    }
+}
+
+fn send_messages(msgs: Vec<Message>) {
+    for msg in msgs {
+        let node_id = msg.to;
+        let addr = get_addr(node_id as usize);
+        match msg.write_to_bytes() {
+            Ok(data) => {
+                if let Err(e) = raft_send(addr, &data) {
+                    eprintln!("Failed to send message to {}: {}", addr, e);
+                }
+            },
+            Err(e) => eprintln!("Failed to serialize message: {}", e),
+        }
+    }
+}
+
+fn get_addr(id: usize) -> &'static str {
+    NODE_LIST[id-1]
+}
+
+fn get_config() -> Config {
+    Config {
+        max_size_per_msg: 1024 * 1024 * 1024,
+        max_inflight_msgs: 512,
+        applied: 0,
+        election_tick: 10,
+        heartbeat_tick: 3,
+        ..Default::default()
+    }
+}
+
+fn handle_committed_entries(node: &mut RawNode<MemStorage>,
+                            store: &MemStorage,
+                            committed_entries: Vec<Entry>,
+                            response_map: &mut HashMap<u64, oneshot::Sender<Option<()>>>
+) {
+    for entry in committed_entries {
+        let last_apply_index = entry.index;
+        if entry.data.is_empty() {
+            continue;
+        }
+        if let Some(sender) = response_map.remove(&last_apply_index) {
+            let _ = sender.send(Some(()));
+        }
+
+        if let EntryType::EntryConfChange = entry.get_entry_type() {
+            let mut cc = ConfChange::default();
+            cc.merge_from_bytes(&entry.data).unwrap();
+            let cs = node.apply_conf_change(&cc).unwrap();
+            store.wl().set_conf_state(cs);
+        } else {
+            println!("{:?}: {:?}",std::time::SystemTime::now().duration_since(UNIX_EPOCH).unwrap(),entry.data);
+        }
+        if node.raft.state == StateRole::Leader {
+            // CLIENT RESPONSE ONE SHOT
+        }
+    }
+}
+
+fn on_ready(node: &mut RawNode<MemStorage>, response_map: &mut HashMap<u64, oneshot::Sender<Option<()>>>) {
+    if !node.has_ready() {
+        return;
+    }
+    let mut ready = node.ready();
+
+    if !ready.messages().is_empty() {
+        send_messages( ready.take_messages());
+    }
+
+    let store = node.raft.raft_log.store.clone();
+    if !ready.snapshot().is_empty() {
+        store.wl().apply_snapshot(ready.snapshot().clone()).unwrap();
+    }
+
+    handle_committed_entries(node, &store, ready.take_committed_entries(), response_map);
+
+    if !ready.entries().is_empty() {
+        store.wl().append(ready.entries()).unwrap();
+    }
+
+    // Hard state
+    if let Some(hs) = ready.hs() {
+        store.wl().set_hardstate(hs.clone());
+    }
+
+    if !ready.persisted_messages().is_empty() {
+        send_messages(ready.take_persisted_messages());
+    }
+
+    let mut light_rd = node.advance(ready);
+    if let Some(commit) = light_rd.commit_index() {
+        store.wl().mut_hard_state().set_commit(commit);
+    }
+    send_messages(light_rd.take_messages());
+    handle_committed_entries(node,  &store, light_rd.take_committed_entries(), response_map);
+    node.advance_apply();
+}
+
+fn create_follower(node_id: u64, logger: &Logger) -> RawNode<MemStorage> {
+    let mut cfg = get_config();
+    cfg.id = node_id;
+    let logger = logger.new(o!("tag" => format!("peer_{}", node_id)));
+    let storage = MemStorage::new();
+    RawNode::new(&cfg, storage, &logger).unwrap()
+}
+
+fn initialize_all_followers(proposer_channel: Sender<Prop>) {
+    for i in 1..NODE_LIST.len()+1 {
+        let mut conf_change = ConfChange::default();
+        conf_change.node_id = i as u64;
+        conf_change.set_change_type(ConfChangeType::AddNode);
+        proposer_channel.send(Prop::Conf(conf_change)).unwrap();
+        thread::sleep(Duration::from_millis(100));
+    }
+}
+
+fn initialize_leader(logger: &Logger) -> RawNode<MemStorage> {
+    let mut cfg = get_config();
+    cfg.id = 1;
+    let logger = logger.new(o!("tag" => format!("peer_{}", 1)));
+    let mut s = Snapshot::default();
+    s.mut_metadata().index = 1;
+    s.mut_metadata().term = 1;
+    s.mut_metadata().mut_conf_state().voters = vec![1];
+    let storage = MemStorage::new();
+    storage.wl().apply_snapshot(s).unwrap();
+    RawNode::new(&cfg, storage, &logger).unwrap()
+}
+
+fn run_node(proposals: Receiver<Prop>, raft_channel: Receiver<Message>, mut node: RawNode<MemStorage>) {
+    let mut t = Instant::now();
+    let timeout = Duration::from_millis(10);
+    let mut response_hashmap: HashMap<u64, oneshot::Sender<Option<()>>> = HashMap::default();
+
+    loop {
+        loop {
+            match raft_channel.recv_timeout(timeout) {
+                Ok(msg) => {
+                    node.step(msg).unwrap();
+                }
+                Err(RecvTimeoutError::Timeout) => break,
+                Err(RecvTimeoutError::Disconnected) => return,
+            }
+        }
+        if t.elapsed() >= Duration::from_millis(100) {
+            // println!("====> TICK: {:?}",std::time::SystemTime::now().duration_since(UNIX_EPOCH).unwrap());
+            node.tick();
+            t = Instant::now();
+        }
+
+        // Only listen to proposals if we become a leader
+        if node.raft.state == StateRole::Leader {
+            match proposals.try_recv() {
+                Ok(msg) => {
+                    match msg {
+                        Prop::Data(data, roneshot) => {
+                            let idx = node.raft.raft_log.last_index() + 1;
+                            node.propose(vec![], data).unwrap();
+                            if node.raft.raft_log.last_index() + 1 > idx {
+                                response_hashmap.insert(idx,roneshot);
+                            }
+                        },
+                        Prop::Conf(conf) => node.propose_conf_change(vec![], conf).unwrap()
+                    }
+                },
+                Err(TryRecvError::Empty) => (),
+                Err(TryRecvError::Disconnected) => return,
+            }
+        }
+
+        on_ready(&mut node, &mut response_hashmap);
+    }
+}
+
+async fn handle_post_request(body: web::Bytes, proposals: web::Data<Sender<Prop>>) -> impl Responder {
+    let data = body.to_vec();
+    let (tx, rx) = oneshot::channel();
+
+    proposals.send(Prop::Data(data,tx)).unwrap();
+    match rx.await {
+        Ok(message) => match message {
+            None => HttpResponse::InternalServerError().body("Error processing request"),
+            Some(_) => HttpResponse::Ok().body("OK"),
+        },
+        Err(e) => {
+            HttpResponse::InternalServerError().body(format!("Error receiving one shot {:?}", e))
+        }
+    }
+}
+
+fn start_web_server(proposals: Sender<Prop>) {
+    let proposals = web::Data::new(proposals);
+
+    let rt = tokio::runtime::Runtime::new().unwrap();
+
+    rt.spawn(async move {
+        let server = HttpServer::new(move || {
+            App::new()
+                .app_data(proposals.clone())
+                .service(web::resource("/post").route(web::post().to(handle_post_request)))
+        })
+            .bind("127.0.0.1:8080")
+            .unwrap()
+            .run();
+
+        let _ = server.await;
+    });
+}
+
+fn main() {
+
+    // Logging
+    let decorator = slog_term::TermDecorator::new().build();
+    let drain = slog_term::FullFormat::new(decorator).build().fuse();
+    let drain = slog_async::Async::new(drain)
+        .chan_size(4096)
+        .overflow_strategy(slog_async::OverflowStrategy::Block)
+        .build()
+        .fuse();
+    let drain = drain.filter_level(Level::Info).fuse();
+    let logger = slog::Logger::root(drain, o!());
+
+    let node_id = u64::from_str(&env::var("NODE_ID").unwrap()).unwrap();
+
+    let (prop_s, prop_r) = mpsc::channel();
+    let (channel_s, channel_r) = mpsc::channel();
+    let listener = thread::spawn(move || raft_listen(get_addr(node_id as usize), channel_s).unwrap());
+
+    let node = if node_id==1 {
+        initialize_leader(&logger)
+    } else {
+        create_follower(node_id, &logger)
+    };
+
+    let m = thread::spawn(move ||
+        run_node(prop_r,channel_r, node)
+    );
+
+
+    if node_id==1 {
+        initialize_all_followers(prop_s.clone());
+        thread::sleep(Duration::from_millis(5000));
+        // let mut prop_vec = vec![];
+        // for i in 1..100000u64 {
+        //         prop_vec.push(i.to_le_bytes());
+        // }
+        // for msg in prop_vec {
+        //     // thread::sleep(Duration::from_millis(1));
+        //     prop_s.send(Prop::Data(msg.to_vec())).unwrap();
+        // }
+
+        start_web_server(prop_s);
+
+    }
+
+
+    m.join().unwrap();
+    listener.join().unwrap();
+
+}
